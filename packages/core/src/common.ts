a4177e186fd001e193f8eec5630f31398533c84c
Mickaël Beguin <mickael.b@bridgeapi.io>
2024-09-06 22:22:22 +0200

    [BUGFIX] Fix cross origin function

--- packages/core/src/common.ts
+++ packages/core/src/common.ts
@@ -2,8 +2,6 @@ import type { Settings } from "~/type";
 
 export const isInIframe = () => window && window.self !== window.top;
 
-export const isSameOriginIframe = (iframe: HTMLIFrameElement): iframe is HTMLIFrameElement & { contentDocument: Document } => !!iframe.contentDocument;
-
 export const isHtmlIframeElement = (element: Element): element is HTMLIFrameElement => element instanceof HTMLIFrameElement;
 
 export const deferWhenWindowIsLoaded = (_window: Window, executable: () => void) => {
@@ -29,8 +27,7 @@ export const getDefaultSettings: () => Settings = () => ({ offsetSize: 0, checkO
 
 export const isIframeSameOrigin = (iframe: HTMLIFrameElement) => {
   try {
-    const iframeSrc = new URL(iframe.src, window.location.origin);
-    return iframeSrc.origin === window.location.origin;
+    return new URL(iframe.src).origin === window.location.origin;
   } catch (e) {
     return false;
   }
