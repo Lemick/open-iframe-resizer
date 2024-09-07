c93bd08a315c2dbbdf97fd3275e200dab0afa6fe
Mickaël B <sidfloyd84@gmail.com>
2024-09-07 13:17:59 +0200

    [BUGFIX] Fix cross origin function (#2)

--- packages/core/src/compat.ts
+++ packages/core/src/compat.ts
@@ -1,20 +1,18 @@
 // Backward compatibility with iframe-resizer lib
 
-import { extractIframeOrigin, safePostMessageToCrossOriginIframe } from "~/common";
+import { safePostMessageToCrossOriginIframe } from "~/common";
 
 export function sendLegacyLibInitMessageOnIframeLoad(iframe: HTMLIFrameElement) {
   safePostMessageToCrossOriginIframe(iframe, () =>
-    iframe.contentWindow?.postMessage(
-      "[iFrameSizer]ID:0:false:false:32:true:true::auto:::0:false:child:auto:true:::true:::false",
-      extractIframeOrigin(iframe) ?? "*",
-    ),
+    iframe.contentWindow?.postMessage("[iFrameSizer]ID:0:false:false:32:true:true::auto:::0:false:child:auto:true:::true:::false", "*"),
   );
 }
 
 export function handleLegacyLibResizeMessage(event: MessageEvent): number | null {
   if (typeof event.data === "string" && event.data.startsWith("[iFrameSizer]")) {
-    const [_, height] = event.data.split(":");
-    return +height;
+    const [_, heightStr] = event.data.split(":");
+    const height = +heightStr;
+    return height > 0 ? height : null;
   }
   return null;
 }
