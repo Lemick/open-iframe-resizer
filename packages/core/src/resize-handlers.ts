4646d6dece93769a2745f5dac8eff73672633c48
Mickaël Beguin <sidfloyd84@gmail.com>
2024-11-11 12:10:46 +0100

    [CHORE] Update version

--- packages/core/src/resize-handlers.ts
+++ packages/core/src/resize-handlers.ts
@@ -2,7 +2,9 @@ import type { ResizeContext } from "~/type";
 
 /**
  * Resize handler that scrolls to restore the iframe's position in the viewport as it was before the resize.
- * Note: This behavior only triggers if the iframe is currently being hovered by the user, in order to preserve the user experience during interaction.
+ *
+ * *Note:* This behavior only triggers if the iframe is currently being hovered by the user,
+ * in order to try to limit the number of scroll as it can affect the user experience.
  */
 export const updateParentScrollOnResize = ({ previousRenderState, nextRenderState, interactionState }: ResizeContext) => {
   if (interactionState.isHovered) {
