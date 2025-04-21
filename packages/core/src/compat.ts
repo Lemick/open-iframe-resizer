3b5b191106a03a5d95aa491f4b3e14f49e7381f3
Mickaël Beguin <sidfloyd84@gmail.com>
2025-04-21 15:54:38 +0200

    [BUGFIX] Ignore some legacy events

--- packages/core/src/compat.ts
+++ packages/core/src/compat.ts
@@ -3,10 +3,15 @@
 export const getLegacyLibInitMessage = () => "[iFrameSizer]ID:0:false:false:32:true:true::auto:::0:false:child:auto:true:::true:::false";
 
 export function handleLegacyLibResizeMessage(event: MessageEvent): number | null {
-  if (typeof event.data === "string" && event.data.startsWith("[iFrameSizer]")) {
-    const [_, heightStr] = event.data.split(":");
-    const height = +heightStr;
-    return height > 0 ? height : null;
+  if (typeof event.data !== "string" || !event.data.startsWith("[iFrameSizer]")) {
+    return null;
   }
-  return null;
+
+  if (!event.data.endsWith("mutationObserver") && !event.data.endsWith("resizeObserver")) {
+    return null;
+  }
+
+  const [_, heightStr] = event.data.split(":");
+  const height = +heightStr;
+  return height > 0 ? height : null;
 }
